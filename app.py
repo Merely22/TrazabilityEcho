f4c34b3246049e3d2c30c2e6b730c2e944144c86
Merely  Tumbalobos <48566091+Merely22@users.noreply.github.com>
2025-07-01 14:45:24 -0500

    add observation item and improve the plot of produccion process

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 #========================================================================================
 st.set_page_config(page_title="Dashboard Trazability TKI", layout="wide")
 st.title("📊 Trazability TKI - ECHO")
-#========================================================================================
+
 # Autenticación con Google Sheets
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 credentials = service_account.Credentials.from_service_account_info(
@@ -26,7 +26,7 @@ SHEET_NAME = "Echo" # changed pending
 def load_data(): 
     result = service.spreadsheets().values().get(
         spreadsheetId=SPREADSHEET_ID,
-        range=f"{SHEET_NAME}!A2:AD100" # changed pending
+        range=f"{SHEET_NAME}!A2:AD500" # changed pending
     ).execute()
 
     values = result.get("values", [])
@@ -54,55 +54,67 @@ df.rename(columns={
     'Testing_Date01': 'Date_NMEA_QC1',
     'Testing_Date02': 'Date_NMEA_QC2',
     'Production Date': 'Date_Prod',
-    'Shippent Date': 'Date_Shipp' 
+    'Shippent Date': 'Date_Shipp',
+    'Observations': 'Observations' # add observations column
 }, inplace=True)
 
-# --- PASO 2: Eliminar filas donde la MAC es nula o vacía, ya que no representan un equipo válido.
+# --- PASO 2: Eliminar filas donde la MAC es nula
 df.dropna(subset=['MAC'], inplace=True)
 df = df[df['MAC'].str.strip() != '']
 df['MAC'] = df['MAC'].astype(str).str.strip()
 
 # --- PASO 3: Convertir las columnas de fecha a formato de fecha ---
+date_columns = ['Date_Test_Lab', 'Date_NMEA_QC1', 'Date_NMEA_QC2', 'Date_Prod', 'Date_Shipp']
+for col in date_columns:
+    df[col] = pd.to_datetime(df[col], errors='coerce', dayfirst=True)
 
-#df['Date_Test_Lab'] = pd.to_datetime(df['Date_Test_Lab'], format='%d/%m/%Y', errors='coerce')
-#df['Date_NMEA_QC1'] = pd.to_datetime(df['Date_NMEA_QC1'], format='%d-%m-%y', errors='coerce')
-#df['Date_NMEA_QC2'] = pd.to_datetime(df['Date_NMEA_QC2'], format='%d-%m-%y', errors='coerce')
-#df['Date_Prod'] = pd.to_datetime(df['Date_Prod'], format='%d-%m-%y', errors='coerce')
-#df['Date_Shipp'] = pd.to_datetime(df['Date_Shipp'], format='%d-%m-%y', errors='coerce')
+#  build a dictionary with observations
+observaciones = [
+    'does not charge', 'restart', 'recognized', 
+    'no charge iphone', 'no com port detected', 'no satelites in view']
 
-df['Date_Test_Lab'] = pd.to_datetime(df['Date_Test_Lab'], errors='coerce', dayfirst=True)
-df['Date_NMEA_QC1'] = pd.to_datetime(df['Date_NMEA_QC1'], errors='coerce', dayfirst=True)
-df['Date_NMEA_QC2'] = pd.to_datetime(df['Date_NMEA_QC2'], errors='coerce', dayfirst=True)
-df['Date_Prod'] = pd.to_datetime(df['Date_Prod'], errors='coerce', dayfirst=True)
-df['Date_Shipp'] = pd.to_datetime(df['Date_Shipp'], errors='coerce', dayfirst=True)
+# build a df and clean column observations 
+df['Observations_lower'] = df['Observations'].str.lower().fillna('')
 
+# build a function 
+def find_word(texto):
+    for palabra in observaciones:
+        if palabra in texto:
+            return palabra.capitalize() 
+    return np.nan # Devolvemos NaN si no se encuentra ninguna
+
+# build the column 'Estado_Observacion' applied the fuction
+df['Estado_Observacion'] = df['Observations_lower'].apply(find_word)
 
 # --- PASO 4: Asignamos la etapa de forma secuencial. La última condición que se cumpla será la etapa final.
 df['Etapa_Actual'] = '0. Pendiente' # Valor por defecto
-df.loc[df['Date_Test_Lab'].notna(), 'Etapa_Actual'] = '1. Pruebas de Laboratorio'
-df.loc[df['Date_NMEA_QC1'].notna(), 'Etapa_Actual'] = '2. NMEA QC 01'
-df.loc[df['Date_NMEA_QC2'].notna(), 'Etapa_Actual'] = '3. NMEA QC 02'
-df.loc[df['Date_Prod'].notna(), 'Etapa_Actual'] = '4. Produccion Finalizada'
-df.loc[df['Date_Shipp'].notna(), 'Etapa_Actual'] = '5. Equipos Enviados'
+df.loc[df['Date_Test_Lab'].notna(), 'Etapa_Actual'] = 'Pruebas de Laboratorio'
+df.loc[df['Date_NMEA_QC1'].notna(), 'Etapa_Actual'] = 'NMEA QC 01'
+df.loc[df['Date_NMEA_QC2'].notna(), 'Etapa_Actual'] = 'NMEA QC 02'
+df.loc[df['Date_Prod'].notna(), 'Etapa_Actual'] = 'Produccion Finalizada'
+df.loc[df['Date_Shipp'].notna(), 'Etapa_Actual'] = 'Equipos Enviados'
 
 # --- PASO 5: Calcular la duración en días entre cada etapa ---
 df['Dias_Lab_a_NMEA1'] = (df['Date_NMEA_QC1'] - df['Date_Test_Lab']).dt.days
 df['Dias_NMEA1_a_NMEA2'] = (df['Date_NMEA_QC2'] - df['Date_NMEA_QC1']).dt.days
 df['Dias_NMEA2_a_FinProd'] = (df['Date_Prod'] - df['Date_NMEA_QC2']).dt.days
-df['Dias_Prod_Shipp'] = (df['Date_Prod'] - df['Date_Shipp']).dt.days
+df['Dias_Prod_Shipp'] = (df['Date_Shipp'] - df['Date_Prod']).dt.days
 df['Dias_Totales'] = (df['Date_Shipp'] - df['Date_Test_Lab']).dt.days
+
+# build a df with the main observation
+df_obs = df.dropna(subset=['Estado_Observacion'])
 #========================================================================================
+
 # --- KPIs / Resumen General ---
-st.header("Resumen General del Proceso")
-
-# Contar equipos en cada etapa (considerando la última completada)
+# Contar equipos en cada etapa 
 total_equipos = len(df)
-en_etapa1 = len(df[df['Etapa_Actual'] == '1. Pruebas de Laboratorio'])
-en_etapa2 = len(df[df['Etapa_Actual'] == '2. NMEA QC 01'])
-en_etapa3 = len(df[df['Etapa_Actual'] == '3. NMEA QC 02'])
-en_etapa4 = len(df[df['Etapa_Actual'] == '4. Produccion Finalizada'])
-en_etapa5 = len(df[df['Etapa_Actual'] == '5. Equipos Enviados'])
+en_etapa1 = len(df[df['Etapa_Actual'] == 'Pruebas de Laboratorio'])
+en_etapa2 = len(df[df['Etapa_Actual'] == 'NMEA QC 01'])
+en_etapa3 = len(df[df['Etapa_Actual'] == 'NMEA QC 02'])
+en_etapa4 = len(df[df['Etapa_Actual'] == 'Produccion Finalizada'])
+en_etapa5 = len(df[df['Etapa_Actual'] == 'Equipos Enviados'])
 
+st.header ('Resumen de Equipos por Etapa')
 col1, col2, col3, col4, col5, col6 = st.columns(6)
 col1.metric("Total de Equipos", f"{total_equipos}")
 col2.metric("En Lab Test", f"{en_etapa1}")
@@ -111,8 +123,35 @@ col4.metric("En NMEA QC 2", f"{en_etapa3}")
 col5.metric("Produccion finalizada", f"{en_etapa4}")
 col6.metric("Enviados", f"{en_etapa5}")
 
+st.divider()
+
+# --- Add table with observations ---  
+col_obs1, col_obs2 = st.columns(2)
+with col_obs1:
+    st.subheader ('Equipos en estado de Observación')
+    count_obs = df_obs['Estado_Observacion'].value_counts().reset_index()     # observation counts
+    count_obs.columns = ['Tipo de Problema', 'Cantidad']
+    fig_obs = px.bar(
+        count_obs,
+        x='Cantidad',
+        y='Tipo de Problema',
+        orientation='h',
+        text='Cantidad'
+    )
+    fig_obs.update_layout(yaxis={'categoryorder':'total ascending'})
+    st.plotly_chart(fig_obs, use_container_width=True)
+
+with col_obs2:
+    st.subheader("Estado actual de equipos Observados")
+    st.dataframe(
+        df_obs[['MAC', 'BATCH', 'Estado_Observacion', 'Etapa_Actual', 'Observations']],
+        use_container_width=True
+    )
+
+st.divider()
+
 # --- Análisis de Tiempos ---
-st.header("⏱️ Análisis de Tiempos de Procesamiento")
+#st.header("⏱️ Análisis de Tiempos de Producción")
 col1_tiempo, col2_tiempo = st.columns(2)
 
 with col1_tiempo:
@@ -123,7 +162,7 @@ with col1_tiempo:
     avg_dias_4_5 = df['Dias_Prod_Shipp'].mean()
     avg_dias_total = df['Dias_Totales'].mean()
 
-    # Mostramos los promedios solo si se pueden calcular (si hay datos)
+    # Mostramos los promedios 
     if pd.notna(avg_dias_1_2):
         st.info(f"**Promedio Lab → NMEA 1:** {avg_dias_1_2:.1f} días")
     if pd.notna(avg_dias_2_3):
@@ -136,31 +175,29 @@ with col1_tiempo:
         st.success(f"**Promedio Total Proceso:** {avg_dias_total:.1f} días")
         
 with col2_tiempo:
-    st.subheader("Cantidad de Equipos por Etapa")
-    # Crear gráfico de pastel (porcentaje de equipos por etapa)
-    etapas = [
-        '1. Pruebas de Laboratorio',
-        '2. NMEA QC 01',
-        '3. NMEA QC 02',
-        '4. Produccion Finalizada',
-        '5. Equipos Enviados'
+    st.subheader("Visualización del Flujo de Proceso")
+    etapas_labels = [
+                '1. Pruebas de Laboratorio', '2. NMEA QC 01', '3. NMEA QC 02',
+                '4. Produccion Finalizada', '5. Equipos Enviados'
+            ]
+    etapas_valores = [
+        len(df[df['Date_Test_Lab'].notna()]),
+        len(df[df['Date_NMEA_QC1'].notna()]),
+        len(df[df['Date_NMEA_QC2'].notna()]),
+        len(df[df['Date_Prod'].notna()]),
+        len(df[df['Date_Shipp'].notna()])
     ]
-    colores = ['#c6dbef', '#9ecae1', '#6baed6', '#3182bd', '#08519c']
-
-    valores = [len(df[df['Etapa_Actual'] == etapa]) for etapa in etapas]
-
-    fig = go.Figure(go.Pie(
-        labels=etapas,
-        values=valores,
-        hole=0.4,
-        marker_colors=colores,
-        #textinfo='percent+label',
-        insidetextorientation='radial',
-        pull=[0.05] * len(etapas)
+    
+    fig_funnel = go.Figure(go.Funnel(
+        y = [label.split('. ')[1] for label in etapas_labels], # Nombres más limpios
+        x = etapas_valores,
+        textinfo = "value+percent initial"
     ))
-    fig.update_layout(margin=dict(t=10, b=10, l=10, r=10))
-    st.plotly_chart(fig, use_container_width=True)
+    fig_funnel.update_layout(margin=dict(t=30, b=10, l=10, r=10))
+    st.plotly_chart(fig_funnel, use_container_width=True)
 
+
+st.divider()
 
 # --- Detalle por Etapa ---
 st.header("📋 Estado de los Equipos por Etapa")
@@ -177,27 +214,27 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs([
 with tab1:
 
     st.subheader("Equipos en Pruebas de Laboratorio")
-    df_filtrado = df[df['Etapa_Actual'] == '1. Pruebas de Laboratorio']
+    df_filtrado = df[df['Etapa_Actual'] == 'Pruebas de Laboratorio']
     st.dataframe(df_filtrado[['ID', 'MAC', 'BATCH', 'Date_Test_Lab']])
 
 with tab2:
     st.subheader("Equipos en Evaluación NMEA QC 01")
-    df_filtrado = df[df['Etapa_Actual'] == '2. NMEA QC 01']
+    df_filtrado = df[df['Etapa_Actual'] == 'NMEA QC 01']
     st.dataframe(df_filtrado[['ID', 'MAC', 'BATCH', 'Date_NMEA_QC1', 'Dias_Lab_a_NMEA1']])
 
 with tab3:
     st.subheader("Equipos en Evaluación NMEA QC 02")
-    df_filtrado = df[df['Etapa_Actual'] == '3. NMEA QC 02']
+    df_filtrado = df[df['Etapa_Actual'] == 'NMEA QC 02']
     st.dataframe(df_filtrado[['ID', 'MAC', 'BATCH', 'Date_NMEA_QC2', 'Dias_NMEA1_a_NMEA2']])
 
 with tab4:
     st.subheader("Equipos en Producciòn Finalizada")
-    df_filtrado = df[df['Etapa_Actual'] == '4. Produccion Finalizada']
+    df_filtrado = df[df['Etapa_Actual'] == 'Produccion Finalizada']
     st.dataframe(df_filtrado[['ID', 'MAC', 'BATCH', 'Date_Prod', 'Dias_Totales']])
 
 with tab5:
     st.subheader("Equipos Enviados")
-    df_filtrado = df[df['Etapa_Actual'] == '5. Equipos Enviados']
+    df_filtrado = df[df['Etapa_Actual'] == 'Equipos Enviados']
     st.dataframe(df_filtrado[['ID', 'MAC', 'BATCH', 'Date_Prod', 'Dias_Totales']])
 
 # --- Tabla de Datos Completa ---
